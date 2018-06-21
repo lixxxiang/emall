f059824e23cd8aeaa5fe33239502918317834f83
lixxxiang <13051744716@163.com>
2018-06-21 17:06:34 +0800

    1. classify  todo:fenye

--- classify/src/main/java/com/android/lixiang/classify/ui/activity/OpticsActivity.kt
+++ classify/src/main/java/com/android/lixiang/classify/ui/activity/OpticsActivity.kt
@@ -10,10 +10,39 @@ import com.android.lixiang.base.utils.view.AppBarStateChangeListener
 import com.android.lixiang.base.utils.view.StatusBarUtil
 import com.android.lixiang.classify.R
 import kotlinx.android.synthetic.main.activity_optics.*
+import android.support.design.widget.TabLayout
+import android.support.v7.widget.GridLayoutManager
+import android.support.v7.widget.LinearLayoutManager
+import android.view.ViewGroup
+import com.android.lixiang.base.ui.activity.BaseMvpActivity
+import com.android.lixiang.base.utils.view.DimenUtil
+import com.android.lixiang.base.utils.view.GridSpacingItemDecoration
+import com.android.lixiang.classify.presenter.OpticsPresenter
+import com.android.lixiang.classify.presenter.data.bean.SceneSearchBean
+import com.android.lixiang.classify.presenter.injection.component.DaggerClassifyComponent
+import com.android.lixiang.classify.presenter.injection.module.ClassifyModule
+import com.android.lixiang.classify.presenter.view.OpticsView
+import com.android.lixiang.classify.ui.adapter.OpticsAdapter
+import com.orhanobut.logger.Logger
+import kotlinx.android.synthetic.main.content_scrolling.*
 
 
-class OpticsActivity : AppCompatActivity() {
+class OpticsActivity : BaseMvpActivity<OpticsPresenter>(), OpticsView {
+    override fun injectComponent() {
+        DaggerClassifyComponent.builder().activityComponent(activityComponent)
+                .classifyModule(ClassifyModule())
+                .build().inject(this)
+    }
+
+    override fun onSceneSearchResult(res: SceneSearchBean) {
+        sceneSearchDataList = res.data.searchReturnDtoList
+        mClassifyRecyclerView.layoutManager = mGridLayoutManager
+        mClassifyRecyclerView.adapter = OpticsAdapter(R.layout.optics_recyclerview_layout,sceneSearchDataList)
+    }
+
     var mActivity: OpticsActivity? = null
+    var sceneSearchDataList: MutableList<SceneSearchBean.DataBean.SearchReturnDtoListBean> = mutableListOf()
+    var mGridLayoutManager: GridLayoutManager? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_optics)
@@ -42,5 +71,37 @@ class OpticsActivity : AppCompatActivity() {
         mToolbar.title = "光学1级"
         setSupportActionBar(mToolbar)
         supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+        initTab()
+        initLayoutManager()
+        getData()
+    }
+
+    private fun initLayoutManager() {
+        mGridLayoutManager = GridLayoutManager(this, 2)
+        mGridLayoutManager!!.isSmoothScrollbarEnabled = true
+        mGridLayoutManager!!.isAutoMeasureEnabled = true
+        mClassifyRecyclerView.addItemDecoration(GridSpacingItemDecoration(2, DimenUtil().dip2px(this, 14F), true))
+        mClassifyRecyclerView.layoutManager = mGridLayoutManager
+        mClassifyRecyclerView.setHasFixedSize(true)
+        mClassifyRecyclerView.isNestedScrollingEnabled = false
+    }
+
+    private fun getData() {
+        mPresenter.mView = this
+        mPresenter.sceneSearch("","","",
+                "","","",
+                "","","10",
+                "1","","android")
+    }
+
+    private fun initTab() {
+        mTabLayout.addTab(mTabLayout.newTab().setText("北京"))
+        mTabLayout.addTab(mTabLayout.newTab().setText("上海"))
+        mTabLayout.addTab(mTabLayout.newTab().setText("Tab 3"))
+        mTabLayout.addTab(mTabLayout.newTab().setText("Tab 4"))
+        mTabLayout.addTab(mTabLayout.newTab().setText("Tab 5"))
+        mTabLayout.addTab(mTabLayout.newTab().setText("Tab 6"))
+        mTabLayout.addTab(mTabLayout.newTab().setText("Tab 7"))
+        mTabLayout.addTab(mTabLayout.newTab().setText("Tab 8"))
     }
 }
